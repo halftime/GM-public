2df00d90a1408cb263b9ee0f03b43391fb805fc3
halftime <60786122+halftime@users.noreply.github.com>
2024-05-24 16:38:29 +0200

    changed to F1 hotkey, alpha darkmode layout changes

--- sportmarket-viewall.js
+++ sportmarket-viewall.js
@@ -3,9 +3,9 @@
 // @version  1
 // @grant    none
 // @namespace    https://pro.sportmarket.com
-// @include https://pro.sportmarket.com/trade/*
-// @include https://pro.sportmarket.com/trade
+// @include https://pro.sportmarket.com/alpha/trade*
 // @run-at       document-end
+// @description sportmarket alpha, darkmode on
 // ==/UserScript==
 
 async function sleep(ms) {
@@ -31,32 +31,50 @@ async function ClickAllDelayed(elementsArray, delayMs = 200) {
     }
 }
 
-window.addEventListener("load", async function (event) {
-    if (document.readyState === "complete") {
-        console.log("dbg: DOM loaded and parsed");
-        await sleep(30000);
-        console.log("dbg: 30s sleep passed");
+document.addEventListener('keydown', async function (event) {
+    if (event.key === 'F1') {
+        if (document.readyState === "complete") {
+            console.log("dbg: DOM loaded and parsed");
 
-        const todayToExpand = document.querySelector('div.MarketSide__MarketCompetitionSideContainer-sc-gkcapd-4.gNMqC.today:not([class$="expanded"]) div'); 
-        const earlyToExpand = document.querySelector('div.MarketSide__MarketCompetitionSideContainer-sc-gkcapd-4.gNMqC.early:not([class$="expanded"]) div');
+            const elements = document.querySelectorAll('div.styles_timeGroupHeader__9wBTz span span.styles_header__PmmkP');
 
-        if (todayToExpand !== null) {
-            todayToExpand.dispatchEvent(clickEvent);
+            if (elements.length != 3) {
+                console.log("dbg: elements.length != 3, exiting...");
+                console.log("dbg: elements: " + elements);
+                return;
+            }
+
+            // elements[0] is "In running" / live
+            const todayToExpand = elements[1];
+            const earlyToExpand = elements[2];
+
+            const todayExpandedBox = document.querySelector('div.styles_listContainer__k_igi.styles_today__zcl7T');
+            const earlyExpandedBox = document.querySelector('div.styles_listContainer__k_igi.styles_early__lbebL');
+
+            console.log("dbg: todayToExpand: " + todayToExpand);
+            console.log("dbg: earlyToExpand: " + earlyToExpand);
+
+            if (todayExpandedBox == null) {
+                todayToExpand.dispatchEvent(clickEvent);
+            }
+
+            await sleep(2000);
+
+            const todayEvents = document.querySelectorAll('div.styles_listContainer__k_igi.styles_today__zcl7T ul.styles_competitionList__sLeBy li.styles_competitionItem__sRPO1:not(.styles_watched__oGXCB)');
+
+            console.log("dbg: todayEvents: " + todayEvents);
+            await ClickAllDelayed(todayEvents, 100);
+
+            await sleep(2000);
+
+            if (earlyExpandedBox == null) {
+                earlyToExpand.dispatchEvent(clickEvent);
+            }
+
+            await sleep(2000);
+            const earlyEvents = document.querySelectorAll('div.styles_listContainer__k_igi.styles_early__lbebL ul.styles_competitionList__sLeBy li.styles_competitionItem__sRPO1:not(.styles_watched__oGXCB)');
+            console.log("dbg: earlyEvents: " + earlyEvents);
+            await ClickAllDelayed(earlyEvents, 100);
         }
-
-        await sleep(2000);
-        const todayEvents = document.querySelectorAll('.MarketSide__Competitions-sc-gkcapd-3 [class$="today  not-suggested normal"]');
-        console.log("dbg: todayEvents: " + todayEvents);
-        await ClickAllDelayed(todayEvents, 300);
-
-        await sleep(2000);
-        if (earlyToExpand !== null) {
-            earlyToExpand.dispatchEvent(clickEvent);
-        }
-
-        await sleep(2000);
-        const earlyEvents = document.querySelectorAll('.MarketSide__Competitions-sc-gkcapd-3 [class$="early  not-suggested normal"]');
-        console.log("dbg: earlyEvents: " + earlyEvents);
-        await ClickAllDelayed(earlyEvents, 300);
     }
 });
