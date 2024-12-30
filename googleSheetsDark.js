4151eac553a36e3392cfccaa9fc164fee9c82c89
halftime <60786122+halftime@users.noreply.github.com>
2024-12-30 23:51:24 +0100

    simple darktheme google sheets

---
+++ googleSheetsDark.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name     Googlesheets Dark
+// @version  0.2
+// @grant    none
+// @description  Simple dark theme for Google Sheets
+// @namespace    https://docs.google.com
+// @include https://docs.google.com/spreadsheets/d/*
+// @run-at       document-start
+// ==/UserScript==
+
+const style = document.createElement('style');
+style.textContent = "[class$='container'] { background-color: rgba(0, 0, 0, 0.33); }";
+style.textContent += "[class^='docs-'] { background-color: rgba(0, 0, 0, 0.33); }";
+
+document.head.appendChild(style);
+
+console.log('Google Sheets Dark Theme loaded');
